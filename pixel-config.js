bfffdc168ba74396feb0014aff230e488fba3572
frog-sand <frogsand4@gmail.com>
2026-01-08 17:10:36 +0530

    capi

--- pixel-config.js
+++ pixel-config.js
@@ -16,14 +16,14 @@ const PIXEL_CONFIG = {
 
   // Blue Landing Page
   'Blue': {
-    pixelId: '25618509431149818', // Same as Black or different
-    accessToken: 'YOUR_BLUE_ACCESS_TOKEN_HERE'
+    pixelId: '844275131701075', // Same as Black or different
+    accessToken: 'EAALyBkjcGVoBQfxfoqIsQ0Ih8kFLJcjhbjRzkWNymyCAQ0WBC2iZBuR0ZA8agiJrPJAOs1YUAz6ltj3W0E4QQZBFJjT7YE569bQ6W0a9OZBaww7y2f2eaWfckDZBM97D1mOwN57ZB4dBhZBAUeokEEKHMGIQbn1sBVV0jf4rf8tC7UEzWYW17FADPNXpazTtdMavgZDZD'
   },
 
   // Green Landing Page
   'Green': {
-    pixelId: '25618509431149818',
-    accessToken: 'YOUR_GREEN_ACCESS_TOKEN_HERE'
+    pixelId: '1401092681649064',
+    accessToken: 'EAALyBkjcGVoBQbhSOqHPAUswPr99lx5hdF2jOHQbjuU3jWrowKiGLAiHaLkuRXpkqhXu3FxEAQusls2wtnei78ZCdo6IezMZBDUXZB0Wq3ZB1lnTDAZClu0PgZByHmiJkwYi7Iw8kjfd5N7tPdUe4lVZB5pzZCRUCDNoHUyaAa9qtiFssXvwBsZBOtzagwuLg9mn89QZDZD'
   },
 
   // Grey Landing Page
@@ -34,14 +34,14 @@ const PIXEL_CONFIG = {
 
   // Peach Landing Page
   'Peach': {
-    pixelId: '25618509431149818',
-    accessToken: 'YOUR_PEACH_ACCESS_TOKEN_HERE'
+    pixelId: '3340672712761892',
+    accessToken: 'EAARW0zbv2DMBQerrfedJK0ZBXdIZCZCgOE13FvXKtXPpn3AODzTZC6xULTFKg6weH1NrJv081X84SGluwchiQLg1TLulQKgSWwlg2QEBDtwKtwQulxibZBcZBFiLixxyNcfz3e3UPToFKcOszzWZCQKSHbb0Xt0llnZAZAG1kkhDg3CGkBvQOR4xrzM9SNX1gnU2ggAZDZD'
   },
 
   // Pink Landing Page
   'Pink': {
-    pixelId: '25618509431149818',
-    accessToken: 'YOUR_PINK_ACCESS_TOKEN_HERE'
+    pixelId: '1759686538046553',
+    accessToken: 'EAARW0zbv2DMBQQ2Tc6FOVZBSAlEnswD8elztyZC6GYzkMm45Cqs2AIaTLvRmWIYb7xpmpEyuZBgDJeXiUqKNIFpOcgZB8vdN8hvi5Dh6lYLw7YtKnynexhtO5CmDtdNcXfPFKJUKZC5ujOJWIFZAva570wNf3HbZBInMLAZBcomttDhty7NxFqLrRYOsr6MPiTWsDwZDZD'
   },
 
   // Purple Landing Page
